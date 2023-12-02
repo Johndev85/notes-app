94a375984a956830774ec0947a6e4f4cae1b9978
JohnB <jhondark@gmail.com>
2023-12-01 20:37:10 -0500

    Update dependencies and add CORS middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import express from "express"
+import cors from "cors"
 import morgan from "morgan"
 
 import notesRoutes from "./routes/notes.routes.js"
+import { FRONTEND_URL } from "./config.js"
 
 const app = express()
+
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+  })
+)
 
 app.use(morgan("dev"))
 app.use(express.json())
