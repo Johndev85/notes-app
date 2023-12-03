ac8b799785ead1b840a855b774313e3fe98a35f6
JohnB <jhondark@gmail.com>
2023-12-03 13:22:26 -0500

    Add resolve import and update app.js configuration

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import { resolve } from "path"
 import morgan from "morgan"
 
 import notesRoutes from "./routes/notes.routes.js"
@@ -7,14 +8,11 @@ import { FRONTEND_URL } from "./config.js"
 
 const app = express()
 
-app.use(
-  cors({
-    origin: FRONTEND_URL,
-  })
-)
-
+app.use(cors())
 app.use(morgan("dev"))
-app.use(express.json())
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(resolve("client/dist")))
 
 app.use("/api", notesRoutes)
 
